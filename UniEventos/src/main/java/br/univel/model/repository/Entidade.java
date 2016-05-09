bcb21e0163374845c58a2b955cf729912f4e5194
Aureo Júnior <aureojr1@gmail.com>
2016-05-09 01:23:07 -0300

    Interface para definir padrões das entidades

---
+++ UniEventos/src/main/java/br/univel/model/repository/Entidade.java
@@ -0,0 +1,33 @@
+package br.univel.model.repository;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *  Gerenciamentos de Eventos da UNIVEL
+ * 
+ * Interface Entidade para marcar as classes que são entidades  do sistema
+ * para garantir a implentação de alguns campos 
+ * 
+ * <li> Id </li>
+ * <li> created At </li>
+ * <li> updated At </li>
+ * 
+ * @author aureo
+ * @since 8/05/2016 21:02
+ *
+ */
+public interface Entidade extends Serializable{
+
+	public int getId() ;
+	
+	public void setId(int id);
+	
+	public Date getCreatedAt() ;
+
+	public void setCreatedAt(Date createdAt) ;
+	
+	public Date getUpdatedAt();
+	
+	public void setUpdatedAt(Date updatedAt);	
+}
