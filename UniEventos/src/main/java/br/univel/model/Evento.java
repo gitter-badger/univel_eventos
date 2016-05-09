e54c33c4a05fea81b5a4da342ceab82fe029d45b
Aureo Júnior <aureojr1@gmail.com>
2016-05-09 01:37:50 -0300

    Implementação dos padroões do banco de dados https://tree.taiga.io/project/aureo-evetos-univel/wiki/banco-de-dados

--- UniEventos/src/main/java/br/univel/model/Evento.java
+++ UniEventos/src/main/java/br/univel/model/Evento.java
@@ -1,88 +1,149 @@
 package br.univel.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.io.Serializable;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Column;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Version;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Entity 
-@XmlRootElement
-public class Evento implements Serializable {
+import br.univel.model.repository.Entidade;
 
-	/**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
+/**
+ * 
+ * 	Gerenciador de eventos da UNIVEL
+ * 
+ * 	Entidade Evento, armazena as configurações básicas de um evento
+ * 
+ * @author Juliano Grans
+ * @author Aureo Junior <aureojr1@gmail.com>
+ *
+ */
+@Entity
+@Table(name = "GEV_EVENTO")
+public class Evento implements Entidade {
 
-  @Version
-	@Column(name = "version")
-	private int version;
+	private static final long serialVersionUID = 1L;
 
 	@Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "Evn_Codigo", updatable = false, nullable = false)
-	private Long evnCodigo;
-  
-  @Column(name = "Evn_Nome")
-  private String evnNome;
-  
-  @JoinColumn(name = "EvnCrd_Codigo", referencedColumnName = "Pss_Codigo")
-  @ManyToOne
-  private Pessoa evnCoordenador;
-
-  public Pessoa getEvnCoordenador() {
-    return evnCoordenador;
-  }
-
-  public void setEvnCoordenador(Pessoa evnCoordenador) {
-    this.evnCoordenador = evnCoordenador;
-  }
-
-  public Long getEvnCodigo() {
-    return evnCodigo;
-  }
-
-  public void setEvnCodigo(Long evnCodigo) {
-    this.evnCodigo = evnCodigo;
-  }
-
-  public String getEvnNome() {
-    return evnNome;
-  }
-
-  public void setEvnNome(String evnNome) {
-    this.evnNome = evnNome;
-  }
-
-
-	public Long getEvn_Codigo() {
-    return evnCodigo;
-  }
-
-  public void setEvn_Codigo(Long evn_Codigo) {
-    evnCodigo = evn_Codigo;
-  }
-
-  public int getVersion() {
-		return this.version;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	/**
+	 * 
+	 * Valor total do evento, uma espécie de pacote completo, em casos onde as pessoas 
+	 * pederão comprar dias do evento, ou palestras específicas que não se aplicam esse
+	 * valor.
+	 * 
+	 */
+	@Column(name = "valor_base")
+	private float valorBase;	
+	
+	@OneToOne
+	@JoinColumn(name = "id_organizacao_evento", referencedColumnName ="id")
+	private OrganizacaoEvento organizacaoEvento;
+	
+	@OneToMany
+	@JoinColumn(name = "id_dias_evento", referencedColumnName ="id")
+	private List<DiaEvento> listaDiasEvento;
+	
+	@OneToOne
+	@JoinColumn(name = "id_configuracoes_evento", referencedColumnName ="id")
+	private ConfiguracoesEvento configuracoesEvento;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at")
+	private Date createdAt;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at")
+	private Date updatedAt;
+	
+	public String getNome() {
+		return nome;
 	}
 
-	public void setVersion(final int version) {
-		this.version = version;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
+	public float getValorBase() {
+		return valorBase;
+	}
+
+	public void setValorBase(float valorBase) {
+		this.valorBase = valorBase;
+	}
+
+	public OrganizacaoEvento getOrganizacaoEvento() {
+		return organizacaoEvento;
+	}
+
+	public void setOrganizacaoEvento(OrganizacaoEvento organizacaoEvento) {
+		this.organizacaoEvento = organizacaoEvento;
+	}
+
+	public List<DiaEvento> getListaDiasEvento() {
+		return listaDiasEvento;
+	}
+
+	public void setListaDiasEvento(List<DiaEvento> listaDiasEvento) {
+		this.listaDiasEvento = listaDiasEvento;
+	}
+
+	public ConfiguracoesEvento getConfiguracoesEvento() {
+		return configuracoesEvento;
+	}
+
+	public void setConfiguracoesEvento(ConfiguracoesEvento configuracoesEvento) {
+		this.configuracoesEvento = configuracoesEvento;
+	}
 
 	@Override
 	public String toString() {
-		String result = getClass().getSimpleName() + " ";
-		if (evnCodigo != null)
-			result += "Evn_Codigo: " + evnCodigo;
-		return result;
+		return "Eventos ";
 	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public void setId(int id) {
+		this.id = id;		
+	}
+
+	@Override
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	@Override
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+   
+	@Override
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@Override
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
 }
