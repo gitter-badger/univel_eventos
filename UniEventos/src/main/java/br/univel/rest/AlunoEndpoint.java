f37781578c6cec78a93f6045e8ecb6a3ac3d448e
Aureo Júnior <aureojr1@gmail.com>
2016-05-09 01:36:00 -0300

    Organização dos pacotes ;p

--- UniEventos/src/main/java/br/univel/rest/AlunoEndpoint.java
+++ UniEventos/src/main/java/br/univel/rest/AlunoEndpoint.java
@@ -1,26 +1,21 @@
 package br.univel.rest;
 
 import java.util.List;
-import java.util.jar.Attributes.Name;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
+
 import br.univel.model.Aluno;
 
 /**
